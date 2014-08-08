b1c7ab46da9a26613fcbc09619f67324b6cbfaba
whitia <whitia7@gmail.com>
2014-08-08 18:05:47 +0900

    Update ws.js

--- ws.js
+++ ws.js
@@ -1,34 +1,51 @@
-function Search(word, properties){
+function Search(word){
 	var url = "http://ja.wikipedia.org/wiki/" + word;
 
 	$("#word").val(word);
-	$("#history").append("<a href=javascript:Search('" + word + "',['title','body','links'])>" + word + "</a> > ");
+	$("#history").append("<a href=javascript:Search('" + word + "')>" + word + "</a> > ");
 	
 	$("#title").empty();
+	$("#contents").empty();
+	$("#image").empty();
 	$("#body").empty();
 	$("#links").empty();
 
-	// 項目名
-	if($.inArray("title", properties) != -1){
-		Get_title(url);
-	}
-	
-	// 本文(見出し/段落/リスト)
-	if($.inArray("body", properties) != -1){
-		Get_body(url);
-	}
-	
-	// 本文内のリンク項目
-	if($.inArray("links", properties) != -1){
-		Get_links(url);
-	}
+	$("input[type='checkbox']").each(function(){
+		switch($(this).val()){
+			case "title":		// 項目名
+				if($(this).prop("checked")){
+					Get_title(url);
+				}
+				break;
+			// case "contents":	// 目次
+			// 	if($(this).prop("checked")){
+			// 		Get_contents(url);
+			// 	}
+			// 	break;
+			case "image":		// 画像
+				if($(this).prop("checked")){
+					Get_image(url);
+				}
+				break;
+			case "body":		// 本文(見出し/段落/リスト)
+				if($(this).prop("checked")){
+					Get_body(url);
+				}
+				break;
+			case "links":		// 本文内のリンク項目
+				if($(this).prop("checked")){
+					Get_links(url);
+				}
+				break;
+		}
+	});
 	
 	// ダウンロード可否判定
 	if(typeof Blob == "undefined"){
 		$("#download").val("このブラウザには対応していません。");
 	}
 
-	// 名前をつけてファイルに保存(おまけ)
+	// 名前をつけて保存
 	$.ajax({
 		url : url,
 		type : "GET",
@@ -59,6 +76,38 @@ function Get_title(url){
 	});
 }
 
+function Get_contents(url){
+	var contents = "";
+
+	$.ajax({
+		url : url,
+		type : "GET",
+		success : function(data){
+			contents = $(data.responseText).find("#toc").html();
+
+			$("#contents").append(contents);
+		}
+	})
+}
+
+function Get_image(url){
+	var image = "";
+
+	$.ajax({
+		url : url,
+		type : "GET",
+		success : function(data){
+			image = $(data.responseText).find(".infobox .image img").attr("src");
+
+			if(image){
+				image = "http:" + image;
+
+				$("#image").append("<img src='" + image + "' />");
+			}
+		}
+	});
+}
+
 function Get_body(url){
 	var body = new Array();
 
@@ -66,7 +115,7 @@ function Get_body(url){
 		url : url,
 		type : "GET",
 		success : function(data){
-			// 本文(見出し/段落/リスト)のセレクターを列挙
+			// 本文(見出し/段落/リスト)のセレクター列挙
 			var selector = "#mw-content-text > :header, \
 				#mw-content-text > p, \
 				#mw-content-text > dl > dt, \
@@ -81,16 +130,22 @@ function Get_body(url){
 				var bodyTagName = body[i].tagName;
 				var bodyTextContent = body[i].textContent;
 
-				if(bodyTagName === "H2" || bodyTagName === "H3" || bodyTagName === "H4"){
-					$("#body").append("<h4>" + bodyTextContent.replace(/\[\s+編集\s+\]/gi, "") + "</h4>");
-				}else if(bodyTagName === "P"){
-					$("#body").append(bodyTextContent.replace(/\s/gi, ""));
-				}else if(bodyTagName === "DT"){
-					$("#body").append("<h4>" + bodyTextContent.replace(/\s/gi, "") + "</h4>");
-				}else if(bodyTagName === "DD"){
-					$("#body").append(bodyTextContent.replace(/\s/gi, ""));
-				}else if(bodyTtagName === "LI"){
-					$("#body").append("<li>" + bodyTextContent.replace(/\s/gi, "") + "</li>");
+				switch(bodyTagName){
+					case "H2" || "H3" || "H4":
+						$("#body").append("<h4>" + bodyTextContent.replace(/\[\s+編集\s+\]/gi, "") + "</h4>");
+						break;
+					case "P":
+						$("#body").append(bodyTextContent.replace(/\s/gi, ""));
+						break;
+					case "DT":
+						$("#body").append("<h4>" + bodyTextContent.replace(/\s/gi, "") + "</h4>");
+						break;
+					case "DD":
+						$("#body").append(bodyTextContent.replace(/\s/gi, ""));
+						break;
+					case "LI":
+						$("#body").append("<li>" + bodyTextContent.replace(/\s/gi, "") + "</li>");
+						break;
 				}
 			}
 		}
@@ -99,11 +154,12 @@ function Get_body(url){
 
 function Get_links(url){
 	var links = new Array();
+
 	$.ajax({
 		url : url,
 		type : "GET",
 		success : function(data){
-			// 本文内のリンク項目のセレクターを列挙
+			// 本文内のリンク項目のセレクター列挙
 			var selector = "#mw-content-text > p > a, \
 				#mw-content-text li > a";
 			
@@ -117,7 +173,7 @@ function Get_links(url){
 				var link = links[i].getAttribute("title");
 				
 				if(link && link.indexOf("存在しないページ") == -1){
-					$("#links").append("<a href=javascript:Search('" + link.replace(/\s/gi, "_") + "',['title','body','links'])>" + link + "</a>、");
+					$("#links").append("<a href=javascript:Search('" + link.replace(/\s/gi, "_") + "')>" + link + "</a> ");
 				}
 			}
 		}
